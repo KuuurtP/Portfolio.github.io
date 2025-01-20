3887a8dd2e056552e8ac6664669318e478d68822
KuuurtP <kurt.poklay@gmail.com>
2025-01-20 19:48:18 +0800

    hello

--- stylesheet/main.js
+++ stylesheet/main.js
@@ -4,7 +4,6 @@ const navMenu = document.getElementById("nav-menu"),
   navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
@@ -12,7 +11,6 @@ if (navToggle) {
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
@@ -24,12 +22,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const themeButton = document.getElementById('theme-button');
   const body = document.body;
 
-  // Check the user's preference for dark mode
   if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       body.classList.add('dark-theme');
   }
 
-  // Toggle between dark and light themes
   themeButton.addEventListener('click', function () {
       body.classList.toggle('dark-theme');
       body.classList.toggle('light-theme');
@@ -119,7 +115,6 @@ prevButton.addEventListener('click', () => {
 });
 
 function updatePagination() {
-  // Implement your logic to update pagination dots based on current content index
 }
 
 // Additional features can be implemented here
@@ -142,8 +137,7 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
     },
   },
 
-  /* mousewheel: true,
-  keyboard: true, */
+
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
@@ -173,7 +167,6 @@ window.addEventListener("scroll", scrollActive);
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const nav = document.getElementById("header");
-  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
   if (this.scrollY >= 80) nav.classList.add("scroll-header");
   else nav.classList.remove("scroll-header");
 }
@@ -182,7 +175,6 @@ window.addEventListener("scroll", scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
   else scrollUp.classList.remove("show-scroll");
 }
@@ -194,19 +186,15 @@ const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "uil-sun";
 
-// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
 
-// We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 
-// We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme
   );
@@ -215,12 +203,9 @@ if (selectedTheme) {
   );
 }
 
-// Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
